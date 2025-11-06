b823f2094d7c0ea4c6540f347e79b38254a6ce9a
Matheus Furtado <furtado.matheus@estudante.ufjf.br>
2025-11-06 15:22:39 -0300

    :rocket: tira o strict e finaliza a aplicaçao

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 //Lembrar de remover o StrictMode ao terminar o projeto
 createRoot(document.getElementById('root')!).render(
-  <StrictMode> 
-    <App />
-  </StrictMode>,
+  <App />
 )
+
