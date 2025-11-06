eaaf873ecc490dabbf5d5de6a4da1de80158ca38
Lucas <lucasguimaraes.reis@estudante.ufjf.br>
2025-11-05 23:24:56 -0300

    :art: Terminando Ajustes do Texto

--- src/Carta.tsx
+++ src/Carta.tsx
@@ -3,29 +3,31 @@ import Texto from './Componentes/Texto';
 
 interface CartaProps{
     nome?: string;
+    alinhanome?: 'esquerda' | 'centro' | 'direita';
+    alinhadescricao?: 'esquerda' | 'centro' | 'direita';
     ataque?: number;
     defesa?: number;
     descricao?: string;
     tamanho?: 'pequena' | 'media' | 'grande';
-    alinha?: 'esquerda' | 'centro' | 'direita';
 }
 
 export default function Carta({
     nome='Nome',
+    alinhanome='centro',
+    alinhadescricao='esquerda',
     ataque=1000,
     defesa=1000,
     descricao='',
-    tamanho='media',
-    alinha='centro',
+    tamanho='pequena',
 }: CartaProps){
     return (
         <div className="carta">
             <div className="Repartedor">
                 <div>
-                    <Texto conteudo={nome} classe='Nome' tamanho='grande' alinhamento={alinha} />
+                    <Texto conteudo={nome} classe='Nome' tamanho='grande' alinhamento={alinhanome} />
                 </div>
                 <div>
-                    <Texto conteudo={descricao} classe='desc' tamanho={tamanho} alinhamento={alinha} />
+                    <Texto conteudo={descricao} classe='desc' tamanho={tamanho} alinhamento={alinhadescricao} />
                 </div>
                 <div className='ataque'>{ataque}</div>
                 <div className='defesa'>{defesa}</div>
