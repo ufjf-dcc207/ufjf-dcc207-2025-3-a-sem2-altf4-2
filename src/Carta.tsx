c1868ec24b3f9066202f7186b4ddad98145c14e7
Lucas <lucasguimaraes.reis@estudante.ufjf.br>
2025-11-06 01:07:41 -0300

    :bug: nao sei oq era

--- src/Carta.tsx
+++ src/Carta.tsx
@@ -1,26 +1,8 @@
 import './Carta.css'
 import './imagem.css'
 import Texto from './Componentes/Texto.tsx';
-<<<<<<< HEAD
 import Imagem from './Componentes/Imagem.tsx';
 import type {CartaData} from './Deck';
-=======
-import Imagem from './Componentes/imagem.tsx';
-
-interface CartaProps{
-    nome?: string;
-    alinhanome?: 'esquerda' | 'centro' | 'direita';
-    alinhadescricao?: 'esquerda' | 'centro' | 'direita';
-    ataque?: number;
-    defesa?: number;
-    descricao?: string;
-    tamanho?: 'pequena' | 'media' | 'grande';
-    urlimagem?: string;
-    corImagem?: boolean;
-    girarImagem?: '0' | '90' | '180' | '270';
-    espelharImagem?: boolean;
-}
->>>>>>> 3d4cdc26ce2f70825809308755bd98dc13000caf
 
 export default function Carta({
     nome='Nome',
